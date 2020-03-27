ab04efc9f50d8c942a3517abf00c9abd7a205570
younghyeong ryu <wangta69@naver.com>
2020-03-27 14:51:43 +0900

    Update coinmarketcapApiService.php

--- src/coinmarketcap/coinmarketcapApiService.php
+++ src/coinmarketcap/coinmarketcapApiService.php
@@ -4,41 +4,44 @@ namespace Pondol\CoinExchange;
 use Pondol\Curl\CurlService;
 //limit requests to no more than 10 per minute.
 class CoinmarketcapApiService {// extends \App\Http\Controllers\Controller
-	protected $api_host = "https://api.coinmarketcap.com/v1";
+	protected $api_host = "https://pro-api.coinmarketcap.com/v1";
     protected $graph_host = "https://graphs2.coinmarketcap.com/currencies";
 
 	//protected $pubkey;
 	//protected $prikey;
 
 	public function __construct() {}
-/*
-    public function set_private($pubkey, $prikey){
-        $this->pubkey = $pubkey;
-        $this->prikey = $prikey;
+    public function set_key($apiKey){
+        $this->apikey = $apiKey;
     }
-    */
+
     /**
      * public Api Call
      * @param $method String POST/GET/DELETE...
      * @param $uri String "/uri"
      * @param $params Array ['body', 'headers']
-     * @return Json Object 
+     * @return Json Object
      */
     public function publicApi($method, $uri, $params=[]){
-        
         $api_url = $this->api_host.$uri;
         $curl = new CurlService();
+        $params['headers'] = [
+          'Accepts: application/json',
+          'X-CMC_PRO_API_KEY: '. $this->apikey
+      ];
+
+      echo 'api_url:'.$api_url.PHP_EOL;
         $curl->request($method, $api_url, $params);//
         return json_decode($curl->body());
     }
-    
+
 
     /**
      * public Api Call
      * @param $coin : bitcoin....
      * @param $startDt : 1488161968000;
      * @param $endDt 1519697968000
-     * @return Json Object 
+     * @return Json Object
      */
     public function publicGraphApi($coin, $startDt, $endDt){
         //https://graphs2.coinmarketcap.com/currencies/bitcoin/1488161968000/1519697968000/
@@ -48,7 +51,7 @@ class CoinmarketcapApiService {// extends \App\Http\Controllers\Controller
         return json_decode($curl->body());
     }
 
-    
-    
 
-}//end class BithumbApiService 
+
+
+}//end class BithumbApiService
